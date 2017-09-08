39ad380a764ef7576fa283f0e89ca4e6e0464b19
justin-lai <justin.th.lai@gmail.com>
2017-09-07 17:18:51 -0700

    Fix and refactor broken code
    Modify code to pass tests

--- server/controllers/user_trainer_join.js
+++ server/controllers/user_trainer_join.js
@@ -6,25 +6,26 @@ module.exports.create = (req, res) => {
   .then(userTrainerJoin => {
     res.status(201).send(userTrainerJoin);
   })
-  .error(error => {
-    res.status(500).send(err);
+  .catch(error => {
+    res.status(500).send(error);
   });
 };
 
 module.exports.delete = (req, res) => {
-  models.UserTrainerJoin.where(req.body)
+  models.UserTrainerJoin.where(req.params)
   .fetch()
   .then(userTrainerJoin => {
     if (!userTrainerJoin) {
       throw userTrainerJoin;
     }
+
     return userTrainerJoin.destroy()
   })
   .then(() => {
     res.sendStatus(200);
   })
   .error(error => {
-    res.status(503).send(err);
+    res.status(503).send(error);
   })
   .catch(() => {
     res.sendStatus(404);
@@ -32,16 +33,17 @@ module.exports.delete = (req, res) => {
 };
 
 module.exports.update = (req, res) => {
-  models.UserTrainerJoin.where(req.body)
+  models.UserTrainerJoin.where(req.params)
   .fetch()
   .then(userTrainerJoin => {
     if (!userTrainerJoin) {
       throw userTrainerJoin;
     }
-    return profile.save(req.body, { method: 'update' });
+
+    return userTrainerJoin.save(req.body, { method: 'update' });
   })
-  .then(() => {
-    res.sendStatus(201);
+  .then(userTrainerJoin => {
+    res.status(201).send(userTrainerJoin);
   })
   .error(error => {
     res.status(500).send(error);
@@ -52,7 +54,7 @@ module.exports.update = (req, res) => {
 };
 
 module.exports.get = (req, res) => {
-  models.UserTrainerJoin.where(req.body)
+  models.UserTrainerJoin.where(req.params)
   .fetchAll()
   .then(userTrainerJoin => {
     res.status(200).send(userTrainerJoin);
