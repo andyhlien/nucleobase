abebe1017bc2a7b8d2a6c437a13ac24764881cce
andyhlien <andyhlien@gmail.com>
2017-09-12 14:49:38 -0700

    Refactor and clean up code
    Add state to dynamically render data based on session
    Create methods to add and delete appointments
    Conform code uniformly

--- client/src/pages/dashboard/schedule/appointmentList/index.js
+++ client/src/pages/dashboard/schedule/appointmentList/index.js
@@ -23,6 +23,8 @@ class ApptList extends React.Component {
         }
       }
     };
+    this.updatePreview = this.updatePreview.bind(this);
+    this.cancelAppointment = this.cancelAppointment.bind(this);
   }
 
   previewAppointment(appointment) {
@@ -31,8 +33,14 @@ class ApptList extends React.Component {
     });
   }
 
+  cancelAppointment() {
+    console.log('CANCEL_APPOINTMENT_FUNCTION');
+  }
+
   render() {
+
     return (
+
       <div style={{
         height: '100%',
         display: 'flex',
@@ -44,6 +52,7 @@ class ApptList extends React.Component {
           width: '100%',
           height: '600px'
         }}>
+
           <div className="col-lg-4 col-sm-4" style={{
             padding: '0',
             height: '100%',
@@ -62,8 +71,24 @@ class ApptList extends React.Component {
           />
         </div>
       </div>
+
     );
+
   }
 }
 
 export default ApptList;
+
+
+// Will need a function to be able to lift the currently selected appointment window into the state to pass it down to the preview component
+
+// Keep in mind that the trip information that is mocked up does not have any referrence
+// to the users who have paid for the appointment, or the user who has created the appointment
+// The preview component could stand to use any of that information to be displayed in a much more fancy way.
+
+
+
+
+
+
+
