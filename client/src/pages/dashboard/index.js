cf2c1b1a4f6d6cb4aa46bfa2f41359d46229fa1d
andyhlien <andyhlien@gmail.com>
2017-09-14 19:09:43 -0700

    Refactor React routing

--- client/src/pages/dashboard/index.js
+++ client/src/pages/dashboard/index.js
@@ -60,7 +60,7 @@ class Dashboard extends React.Component {
           <DashNav />
 
           <Route path={`${this.props.match.url}/schedule`} component={Schedule}/>
-          <Route path={`${this.props.match.url}/trainers`} component={Search}/>
+          <Route path={`${this.props.match.url}/trainers`} component={Trainers}/>
           <Route path={`${this.props.match.url}/profile`} component={Info}/>
           <Route exact path={`${this.props.match.url}`} component={Main}/>
         
